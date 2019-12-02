f0d666dfaadad40542b95d0df4e260a2577c101f
yenifersan <44785957+yenifersan@users.noreply.github.com>
2019-12-02 10:40:56 -0500

    first commit

--- models/dbConexion.js
+++ models/dbConexion.js
@@ -1,10 +1,10 @@
 var mysql = require ('mysql');
 
 var conn = mysql.createConnection ({
-    host: 'localhost',
-    user:'root',
-    password: '',
-    database: 'shopXamarin'
+    host: 'b4f4xi2uozyhpjxdriz5-mysql.services.clever-cloud.com',
+    user:'uvgzwizynlhcvd9c',
+    password:'W0k6Q8vLSwaulOOcfhIO',
+    database:'b4f4xi2uozyhpjxdriz5'
 });
 conn.connect();
 module.exports = conn;
